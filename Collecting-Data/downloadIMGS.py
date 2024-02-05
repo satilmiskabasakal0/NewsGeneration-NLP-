0d839ea64a7ff3585652c94eb3a62bd363e432b9
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-02-05 20:37:08 +0300

    download img file updated, img name changed

--- Collecting-Data/downloadIMGS.py
+++ Collecting-Data/downloadIMGS.py
@@ -2,32 +2,73 @@ import pandas as pd
 import requests
 import os 
 
-def download_img(url, folder, df_name):
-    try:
+def get_data():
+    df_list = []
+
+    data_path = '../Data/news-Data/'
+
+    for path in os.listdir(data_path):
+        if path.split(".")[1] == 'csv':
+            df = pd.read_csv(data_path + path)
+            df_list.append(df)
+
+    data = pd.concat(
+        df_list, ignore_index=False
+    )
+    df = data.copy()
+    df = df.drop(columns=['Unnamed: 0'])
+
+    return df
+
+def download_img(url, content_url, folder):
+    try:    
         response = requests.get(url)
-
         if response.status_code == 200:
-            img_name = os.path.join(folder, os.path.basename(df_name + url))
+            
+            img_type = url.split('.')[-1]
+            
+            content_url = content_url.strip()
+            
+            img_path = f'{folder}/{content_url}.{img_type}' 
 
-            with open(img_name, 'wb') as f:
+            with open(img_path, 'wb') as f:
                 f.write(response.content)
-            return img_name
+
+            return img_path
+        else:
+            return None
 
     except Exception as e:
-        print(f"img couldnot download, :Error : {e}")
+        print(f"Img could not be downloaded,{content_url}.")
+        return None
+
+def main():
+    df = get_data()
+
+    df['img_path'] = None
+
+    folder = '../Data/imgs' 
+
+    if os.path.exists(folder):
+        os.rmdir(path=folder)
+
+    os.makedirs(folder)
+     
+    for i, img_url in enumerate(df['Img_url']):
+
+        try:
+            img = img_url.split('?u=')[1]
+        except:
+            img = img_url.split('?u=')[0]
         
+        content_url = df[df['Img_url'] == img_url]['Content_url'].values[0]
+        content_url = content_url.replace('/', '_')
 
-def main(img_url, df_name):
-    folder = "../Data/imgs"
-    os.makedirs(folder, exist_ok=True)
-    download_img(img_url, folder, df_name)
+        img_path = download_img(img, content_url, folder)
 
-for i in ['data_1.csv']:
-    downloaded_imgs = []
+        if img_path:
+            df.at[i, 'img_path'] = img_path
 
-    df = pd.read_csv('../Data/news-Data/' + i)
-    df['img_paht'] = None
-
-    for a in range(len(df)):
-        img = df['Img_url'][a]
-        main(img, i)
+    df.to_csv('../Data/aaa.csv', index=False)  
+    
+    return df
